e4810e23fc0ee6181c60b5d229593381143464f1
jr1248 <jr1248@scarletmail.rutgers.edu>
2024-08-22 15:59:30 -0400

    fixed space

--- service/routes.py
+++ service/routes.py
@@ -39,7 +39,6 @@ def index():
 # CREATE A NEW ACCOUNT
 ######################################################################
 @app.route("/accounts", methods=["POST"])
-
 def create_accounts():
     """
     Creates an Account
@@ -62,7 +61,6 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 @app.route("/accounts", methods=["GET"])
-
 def list_accounts():
     """
     List all Accounts
@@ -78,7 +76,6 @@ def list_accounts():
 # READ AN ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["GET"])
-
 def get_accounts(account_id):
     """
     Reads an Account
@@ -94,7 +91,6 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
-
 def update_accounts(account_id):
     """
     Update an Account
@@ -113,7 +109,6 @@ def update_accounts(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
-
 def delete_accounts(account_id):
     """
     Delete an Account
